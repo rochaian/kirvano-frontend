41964de95c90979c492378642e99c3e2109e0f21
Ian Rocha <iancrocha@gmail.com>
2024-04-22 10:23:59 -0300

    feat: Configuração inicial com Next, Typescript  e Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello worlds!
     </h1>
-      
+       
     </main>
   );
 }
