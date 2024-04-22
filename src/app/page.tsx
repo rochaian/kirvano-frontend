18d16c32a256b3620545e1ecf31cc2fed43f82fb
Ian Rocha <iancrocha@gmail.com>
2024-04-22 20:57:52 -0300

    feat: Adição dos components ShippingForm e PaymentForm com funcionalidade de togleView

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 'use client'
 import Label from "@/components/atoms/Label";
 import OrderSummary from "@/components/molecules/OrderSummary";
-import LineSvg from '../../public/line.svg';
-import CheckSvg from '../../public/check.svg';
-import CustomSelect from "@/components/atoms/CustomSelect";
-import ValidatedInput from "@/components/atoms/ValidateInput";
-import Button from "@/components/atoms/Button";
+import ShippingPaymentForm from "@/components/molecules/ShippingPaymentForm";
 
 export default function Home() {
 
 
-  // Função para validar email
-  const validateAddress = (address: string): boolean => {
-
-    const addressRegex = /^.{5,}$/; // Simples regex para verificar se o campo possui mais de 5 caracteres
-    return addressRegex.test(address);
-  };
+  
 
 
   return (
@@ -27,72 +18,7 @@ export default function Home() {
 
           <div className="col-span-4 bg-[#f6fafd] px-20 py-10 rounded-lg">
 
-            <div className="flex flex-row items-center text-center justify-center gap-2">
-              <span className="text-[#3182CE] text-[20px] font-medium">Shipping</span>
-              <LineSvg />
-              <CheckSvg />
-              <LineSvg />
-              <span className="text-[#828282] text-[20px] font-medium">Payment</span>
-            </div>
-            <br></br>
-
-            <Label text="Shipping Details" variant="primary" />
-            <br></br>
-            <br></br>
-
-            <div className="flex flex-row justify-between items-center">
-
-              <Label text="Use saved address" variant="tertiary" />
-              <CustomSelect options={[
-                { label: '123 , Electric avenue', value: '1' },
-                { label: 'Opção 2', value: '2' },
-                { label: 'Opção 3', value: '3' },
-              ]} defaultOption="1" />
-            </div>
-
-            <br></br>
-            <Label text="First line of address" variant="quaternary" />
-            <ValidatedInput
-              placeholder="Enter your Address"
-              onValidate={validateAddress}
-            />
-
-            <br></br>
-            <Label text="Street name" variant="quaternary" />
-            <ValidatedInput
-              placeholder="Enter your Street name"
-              onValidate={validateAddress}
-            />
-            <br></br>
-            <div className="grid grid-flow-row-dense grid-cols-7  gap-8 ">
-              <div className="col-span-3">
-                <Label text="Postcode" variant="quaternary" />
-                <ValidatedInput
-                  placeholder="Enter your Street name"
-                  onValidate={validateAddress}
-                />
-              </div>
-
-              <div className="col-span-4">
-                <Label text="Select shipping" variant="quaternary" />
-                <CustomSelect options={[
-                  { label: 'Free delivery', value: '1' },
-                  { label: 'Opção 2', value: '2' },
-                  { label: 'Opção 3', value: '3' },
-                ]} defaultOption="1" />
-              </div>
-
-            </div>
-            <br></br>
-
-            <hr className="border-t-1 border-gray-300"></hr>
-            <br></br>
-
-            <div className="flex justify-end">
-              <Button label="Cancel Order" variant="secondary"></Button>
-
-              <Button label="Payment" variant="primary"></Button>
-            </div>
+            <ShippingPaymentForm />
 
 
 
---
+++ src/components/molecules/ShippingPaymentForm.tsx
@@ -0,0 +1,235 @@
+import { useState } from "react";
+import Label from "@/components/atoms/Label";
+import Button from "@/components/atoms/Button";
+import LineSvg from '../../../public/line.svg';
+import CheckSvg from '../../../public/check.svg';
+import ExpirationSvg from '../../../public/expiration-line.svg';
+import CustomSelect from "@/components/atoms/CustomSelect";
+import ValidatedInput from "@/components/atoms/ValidateInput";
+
+export default function ShippingPaymentForm() {
+
+
+    // Função para validar campo
+    const validateAddress = (address: string): boolean => {
+
+        const addressRegex = /^.{5,}$/; // Simples regex para verificar se o campo possui mais de 5 caracteres
+        return addressRegex.test(address);
+    };
+
+    // Função para cancelar o pedido
+    const onCancelOrder = () => {
+        console.log("Cancel order");
+    };
+
+     // Função para proceder o pagamento
+     const onCompleteOrder = () => {
+        console.log("Payment Order");
+    };
+
+
+
+    const ShippingForm: React.FC<{ onChangeView: () => void }> = ({ onChangeView }) => (
+        <div>
+            <div className="flex flex-row items-center text-center justify-center gap-2">
+                <span
+                    className="text-[#3182CE] text-[20px] font-medium cursor-pointer"
+                    onClick={onChangeView}
+                >
+                    Shipping
+                </span>
+                <LineSvg />
+                <CheckSvg />
+                <LineSvg />
+                <span className="text-[#828282] text-[20px] font-medium">Payment</span>
+            </div>
+
+            <br />
+            <Label text="Shipping Details" variant="primary" />
+            <br />
+            <br />
+
+            <div className="flex flex-row justify-between items-center">
+                <Label text="Use saved address" variant="tertiary" />
+                <CustomSelect
+                    options={[
+                        { label: '123, Electric avenue', value: '1' },
+                        { label: 'Option 2', value: '2' },
+                        { label: 'Option 3', value: '3' },
+                    ]}
+                    defaultOption="1"
+                />
+            </div>
+
+            <br />
+            <Label text="First line of address" variant="quaternary" />
+            <ValidatedInput
+                placeholder="Enter your Address"
+                onValidate={(val) => val.length > 5}
+            />
+
+            <br />
+            <Label text="Street name" variant="quaternary" />
+            <ValidatedInput
+                placeholder="Enter your Street name"
+                onValidate={validateAddress}
+            />
+
+            <br />
+
+            <div className="grid grid-flow-row-dense grid-cols-7 gap-8">
+                <div className="col-span-3">
+                    <Label text="Postcode" variant="quaternary" />
+                    <ValidatedInput
+                        placeholder="Enter your Street name"
+                        onValidate={validateAddress}
+                    />
+                </div>
+
+                <div className="col-span-4">
+                    <Label text="Select shipping" variant="quaternary" />
+                    <CustomSelect
+                        options={[
+                            { label: 'Free delivery', value: '1' },
+                            { label: 'Option 2', value: '2' },
+                            { label: 'Option 3', value: '3' },
+                        ]}
+                        defaultOption="1"
+                    />
+                </div>
+            </div>
+
+            <br />
+
+            <hr className="border-t-1 border-gray-300" />
+            <br />
+
+            <div className="flex justify-end">
+                <Button label="Cancel Order" variant="secondary" onClick={onCancelOrder}/>
+                <Button
+                    label="Payment"
+                    variant="primary"
+                    onClick={onChangeView}
+                />
+            </div>
+        </div>
+    );
+
+
+    const PaymentForm: React.FC<{ onChangeView: () => void }> = ({ onChangeView }) => (
+        <div>
+            <div className="flex flex-row items-center text-center justify-center gap-2">
+                <span
+                    className="text-[#3182CE] text-[20px] font-medium"
+                    onClick={onChangeView}
+                >
+                    Shipping
+                </span>
+                <LineSvg />
+                <CheckSvg />
+                <LineSvg />
+                <span className="text-[#3182CE] text-[20px] font-medium">Payment</span>
+            </div>
+
+            <br />
+            <Label text="Payment Details" variant="primary" />
+            <br />
+            <br />
+
+            <div className="flex flex-row justify-between items-center">
+                <Label text="Use saved card" variant="tertiary" />
+                <CustomSelect
+                    options={[
+                        { label: 'Mastercard ending 234', value: '1' },
+                        { label: 'Opção 2', value: '2' },
+                        { label: 'Opção 3', value: '3' },
+                    ]}
+                    defaultOption="1"
+                />
+            </div>
+
+            <br />
+            <Label text="Name on card" variant="quaternary" />
+            <ValidatedInput
+                placeholder="Enter Name on Card"
+                onValidate={validateAddress}
+            />
+
+            <br />
+            <Label text="Card number" variant="quaternary" />
+            <ValidatedInput
+                placeholder="Enter Card number"
+                onValidate={validateAddress}
+            />
+
+            <br />
+
+            <div className="grid grid-flow-row-dense grid-cols-7 gap-8">
+                <div className="col-span-3">
+                    <Label text="Expiration" variant="quaternary" />
+                    <div className="flex flex-row items-center text-center justify-center gap-2">
+                        <ValidatedInput
+                            placeholder="MM"
+                            onValidate={validateAddress}
+                        />
+                        <div className="flex">
+                            <ExpirationSvg />
+                        </div>
+
+                        <ValidatedInput
+                            placeholder="YY"
+                            onValidate={validateAddress}
+                        />
+                    </div>
+
+                </div>
+
+                <div className="col-span-4">
+                    <Label text="CVC" variant="quaternary" />
+                    <ValidatedInput
+                        placeholder="123"
+                        onValidate={validateAddress}
+                    />
+                </div>
+            </div>
+
+            <br />
+
+            <hr className="border-t-1 border-gray-300" />
+            <br />
+
+            <div className="flex justify-end">
+                <Button
+                    label="Cancel Order"
+                    variant="secondary"
+                    onClick={onCancelOrder}
+                />
+                <Button
+                    label="Complete order"
+                    variant="primary"
+                    onClick={onCompleteOrder}
+                />
+            </div>
+        </div>
+    );
+
+
+
+    const [isShippingForm, setIsShippingForm] = useState(true);
+
+    const toggleView = () => {
+        setIsShippingForm(!isShippingForm);
+    };
+
+    return (
+        <div>
+            {isShippingForm ? (
+                <ShippingForm onChangeView={toggleView} />
+            ) : (
+                <PaymentForm onChangeView={toggleView} />
+            )}
+        </div>
+    );
+}
+
+
