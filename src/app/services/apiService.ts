05096b29163dd9058ce0b5cf089537a8c03415e6
Ian Rocha <iancrocha@gmail.com>
2024-04-23 18:27:00 -0300

    refactor: Desacoplamento da camada de serviços, chamada com a API

--- src/app/services/apiService.ts
+++ src/app/services/apiService.ts
@@ -1,6 +1,5 @@
 import { PaymentFormSchema } from "../types/PaymentFormSchema";
 
-// apiService.ts
 // const API_BASE_URL = 'http://localhost:3000';
 const API_BASE_URL = 'https://kirvano-backend.onrender.com'
 
